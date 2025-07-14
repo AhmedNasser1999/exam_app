ca230d05c86c2745fcf84d38cab50b9a265f42df
Moaz Osama <wwwmoazosama525@gmail.com>
2025-07-14 19:21:37 +0300

    feat: init core and add route app

---
+++ lib/features/home/presentation/view/home_view.dart
@@ -0,0 +1,15 @@
+import 'package:exam_app/core/route/route_name.dart';
+import 'package:flutter/material.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
+  static const String routeName = RouteName.homeView;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('')),
+      body: Container(),
+    );
+  }
+}
---
+++ lib/features/splash/presentation/view/splash_view.dart
@@ -0,0 +1,14 @@
+import 'package:exam_app/core/route/route_name.dart';
+import 'package:flutter/material.dart';
+
+class SplashView extends StatelessWidget {
+  const SplashView({super.key});
+  static const String routeName = RouteName.splashView;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('')),
+      body: Container(),
+    );
+  }
+}
