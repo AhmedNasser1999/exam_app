f7387cc7a9abbc528d63646f1fdb327997929ad7
Ahmed Nasser <ahmed.nasser1935@gmail.com>
2025-07-15 06:20:43 +0300

    feat: create CustomButton widget

---
+++ lib/core/widgets/custom_button.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+  final String text;
+  final VoidCallback onPressed;
+  final double? width;
+  final double? height;
+  final Color? backgroundColor;
+  final Color? textColor;
+  final double borderRadius;
+  final TextStyle? textStyle;
+  final Widget? icon;
+
+  const CustomButton({
+    super.key,
+    required this.text,
+    required this.onPressed,
+    this.width,
+    this.height = 50,
+    this.backgroundColor,
+    this.textColor,
+    this.borderRadius = 12.0,
+    this.textStyle,
+    this.icon,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: width ?? double.infinity,
+      height: height,
+      child: MaterialButton(
+        onPressed: onPressed,
+        elevation: 0,
+        color: backgroundColor ?? Colors.white,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(borderRadius),
+          side: BorderSide(color: backgroundColor ?? const Color(0xff022D82)),
+        ),
+        child: Text(
+          text,
+          style:
+              textStyle ??
+              TextStyle(
+                color: backgroundColor == null
+                    ? Color(0xff022D82)
+                    : Colors.white,
+                fontSize: 16,
+                fontWeight: FontWeight.w600,
+              ),
+        ),
+      ),
+    );
+  }
+}
