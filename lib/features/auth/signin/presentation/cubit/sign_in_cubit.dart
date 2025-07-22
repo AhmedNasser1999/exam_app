eb99d9dd2c61d2e664da80c74528bf5e3198409f
Ahmed Nasser <ahmed.nasser1935@gmail.com>
2025-07-22 09:11:45 +0300

    feat(auth): persist user token to keep user logged in

--- lib/features/auth/signin/presentation/cubit/sign_in_cubit.dart
+++ lib/features/auth/signin/presentation/cubit/sign_in_cubit.dart
@@ -1,3 +1,5 @@
+import 'package:exam_app/features/auth/signin/api/store_user_token.dart';
+import 'package:exam_app/features/auth/signin/domain/entities/user.dart';
 import 'package:exam_app/features/auth/signin/domain/use_cases/signin_usecase.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -6,16 +8,20 @@ import 'sign_in_state.dart';
 
 @injectable
 class SignInCubit extends Cubit<SignInState> {
+  final SigninUseCase signInUseCase;
+  final StoreUserToken storeUserToken;
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
-  final SigninUseCase signInUseCase;
 
-  SignInCubit(this.signInUseCase) : super(SignInInitial());
+  SignInCubit(this.signInUseCase, this.storeUserToken) : super(SignInInitial());
 
   Future<void> signIn(String email, String password) async {
     emit(SignInLoading());
     try {
-      await signInUseCase.execute(email, password);
+      User userToken = await signInUseCase.execute(email, password);
+
+      await storeUserToken.saveToken(userToken.token);
+
       emit(SignInSuccess());
     } catch (e) {
       emit(SignInFailure(e.toString()));
